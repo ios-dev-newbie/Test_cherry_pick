f0970c475c7c73778a28bf3caa758aec193f8291
nikhildevgire <nikhil.devgire@agiliad.com>
2020-07-25 10:34:33 +0530

    test2 component and service added in PR_request branch

--- testDemo/src/app/app.module.ts
+++ testDemo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
